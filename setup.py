f469126bb3d6ebdd036371f282fb1f3dd6a6bef8
Johannes Schmitz <johannes.schmitz1@gmail.com>
2026-05-14 12:52:26 +0200

    Fix MacOS arm64 wheel build and update Linux runner

--- setup.py
+++ setup.py
@@ -196,8 +196,6 @@ setup(
     long_description_content_type="text/markdown",
     packages=["pyclothoids"],
     ext_modules=extensions,
-    install_requires=["pybind11>=2.4"],
-    setup_requires=["pybind11>=2.4"],
     cmdclass={"build_ext": BuildExt},
     zip_safe=False,
 )
